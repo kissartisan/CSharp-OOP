88cfbc8b64fbc35e6fe94fafb68d78ec65af3d5b
Reymark Torres <reymark.torres08.dev@gmail.com>
2019-08-27 16:50:22 +0800

    Demonstrate Hiding Methods instead of Overriding them

---
+++ CSharp_OOP/HidingMethods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_OOP
+{
+    class myBase
+    {
+        //virtual function    
+        public virtual void VirtualMethod()
+        {
+            Console.WriteLine("Virtual method defined in the Base class");
+        }
+
+        public void test()
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+
+    class myDerived : myBase
+    {
+        // hiding the implementation of base class method    
+        public new void VirtualMethod()
+        {
+            Console.WriteLine("Virtual method defined in the Derive class");
+
+            //still access the base class method    
+            base.VirtualMethod();
+            base.test();
+        }
+    }
+
+    class virtualClass
+    {
+        static void Main(string[] args)
+        {
+            // class instance    
+            new myDerived().VirtualMethod();
+            Console.Read();
+        }
+    }
+}
--- CSharp_OOP/VirtualMethod.cs
+++ CSharp_OOP/VirtualMethod.cs
@@ -4,30 +4,31 @@ using System.Text;
 
 namespace CSharp_OOP
 {
-    class myBase
-    {
-        //virtual function    
-        public virtual void VirtualMethod()
-        {
-            Console.WriteLine("Virtual method defined in the Base class");
-        }
-    }
+    //class myBase
+    //{
+    //    //virtual function    
+    //    public virtual void VirtualMethod()
+    //    {
+    //        Console.WriteLine("Virtual method defined in the Base class");
+    //    }
+    //}
 
-    class myDerived : myBase
-    {
-        // redifing the implementation of base class method    
-        public override void VirtualMethod()
-        {
-            Console.WriteLine("Virtual method defined in the Derive class");
-        }
-    }
-    class virtualClass
-    {
-        static void Main(string[] args)
-        {
-            // class instance    
-            new myDerived().VirtualMethod();
-            Console.Read();
-        }
-    }
+    //class myDerived : myBase
+    //{
+    //    // redifing the implementation of base class method    
+    //    public override void VirtualMethod()
+    //    {
+    //        Console.WriteLine("Virtual method defined in the Derive class");
+    //    }
+    //}
+
+    //class virtualClass
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        // class instance    
+    //        new myDerived().VirtualMethod();
+    //        Console.Read();
+    //    }
+    //}
 }
