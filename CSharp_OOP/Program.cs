ed0ba7f622697db8e2b6a6f8b618d78c4bfe765b
Reymark Torres <reymark.torres08.dev@gmail.com>
2019-08-27 13:58:11 +0800

    Demonstrate how to use constructor

---
+++ CSharp_OOP/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSharp_OOP
+{
+    class customer
+    {
+        // Member Variables    
+        public string Name;
+
+        //constructor for initializing fields    
+        public customer(string fname, string lname)
+        {
+            Name = fname + " " + lname;
+        }
+   
+        //method for displaying customer records    
+        public void AppendData()
+        {
+            Console.WriteLine(Name);
+        }
+
+        //Entry point    
+        static void Main(string[] args)
+        {
+            // object instantiation    
+            customer obj = new customer("Barack", "Obama");
+
+            //Method calling    
+            obj.AppendData();
+        }
+    }
+}
