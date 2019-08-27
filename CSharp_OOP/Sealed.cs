54e2775e3255b97cb6c12b58dc499834c591ca35
Reymark Torres <reymark.torres08.dev@gmail.com>
2019-08-27 16:58:52 +0800

    Demonstrate Abstract class

---
+++ CSharp_OOP/Sealed.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_OOP
+{
+    sealed class SealedClass
+    {
+        void myfunv();
+    }
+
+    public class test : SealedClass //wrong. will give compilation error    
+    {
+    }
+}
