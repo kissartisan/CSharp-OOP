eed9f4685561f2f086961e53513db07271d5da78
Reymark Torres <reymark.torres08.dev@gmail.com>
2019-08-27 16:37:50 +0800

    Demonstrate Constructor Inheritance

--- CSharp_OOP/Inheritance.cs
+++ CSharp_OOP/Inheritance.cs
@@ -5,35 +5,35 @@ using System.Text;
 namespace CSharp_OOP
 {
     //Base Class    
-    public class Father
-    {
-        public void FatherMethod()
-        {
-            Console.WriteLine("this property belong to Father");
-        }
-    }
+    //public class Father
+    //{
+    //    public void FatherMethod()
+    //    {
+    //        Console.WriteLine("this property belong to Father");
+    //    }
+    //}
 
-    //Derived class    
-    public class Child : Father
-    {
-        public void ChildMethod()
-        {
-            Console.WriteLine("this property belong to Child");
-        }
-    }
-    class Inheritance
-    {
-        //Entry point    
-        static void Main(string[] args)
-        {
+    ////Derived class    
+    //public class Child : Father
+    //{
+    //    public void ChildMethod()
+    //    {
+    //        Console.WriteLine("this property belong to Child");
+    //    }
+    //}
+    //class Inheritance
+    //{
+    //    Entry point
+    //    static void Main(string[] args)
+    //    {
 
-            Father fObj = new Father();
-            fObj.FatherMethod();
+    //        Father fObj = new Father();
+    //        fObj.FatherMethod();
 
-            //Here Child object can access both class methods    
-            Child cObj = new Child();
-            cObj.FatherMethod();
-            cObj.ChildMethod();
-        }
-    }
+    //        //Here Child object can access both class methods    
+    //        Child cObj = new Child();
+    //        cObj.FatherMethod();
+    //        cObj.ChildMethod();
+    //    }
+    //}
 }
---
+++ CSharp_OOP/InheritanceConstructor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_OOP
+{
+    //Base Class    
+    public class Father
+    {
+
+        //constructor    
+        public Father(string additionalWord = "test")
+        {
+            Console.WriteLine("Father class constructor " + additionalWord);
+        }
+
+        public void FatherMethod()
+        {
+            Console.WriteLine("this property belong to Father");
+        }
+    }
+
+    //Derived class    
+    public class Child : Father
+    {
+        public Child()
+            : base("etc...")
+        {
+            Console.WriteLine("Child class constructor");
+        }
+        public void ChildMethod()
+        {
+            Console.WriteLine("this property belong to Child");
+        }
+    }
+    class Inheritance
+    {
+        //Entry point    
+        static void Main(string[] args)
+        {
+            //Here Child object can access both class methods    
+            Child cObj = new Child();
+            cObj.FatherMethod();
+            cObj.ChildMethod();
+            Console.Read();
+        }
+    }
+}
